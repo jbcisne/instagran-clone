57184d3c14586a6df7db60706f53d3cc9be23c1b
Juliano Buzanello <jbcisne@gmail.com>
2018-09-20 13:49:02 -0300

    Inicio da implementação de animações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,9 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
